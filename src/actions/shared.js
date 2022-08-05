c9eec910457746f284ef7f80b13b22b224c98963
Mohammad Ahmed <yashminahmed@Yashmins-MacBook-Air.local>
2022-08-05 19:26:49 +0530

    Implementing Loadingbar

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -2,15 +2,18 @@ import { getInitialData } from "../utils/api";
 import { receiveUsers } from "./users";
 import { receiveTweets } from "./tweets";
 import { setAuthUser } from "./authUser";
+import { showLoading, hideLoading } from "react-redux-loading-bar";
 
 const AUTH_ID = 'tylermcginnis';
 
 export function handleInitialData() {
     return (dispatch) => {
+        dispatch(showLoading());
         return getInitialData().then(({users, tweets}) => {
             dispatch(receiveUsers(users));
             dispatch(receiveTweets(tweets));
             dispatch(setAuthUser(AUTH_ID));
+            dispatch(hideLoading());
         })
     }
 }
