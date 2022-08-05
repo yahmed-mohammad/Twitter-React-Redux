3091938ab89a0f6fca33d5e964d173b8a9124f05
Mohammad Ahmed <yashminahmed@Yashmins-MacBook-Air.local>
2022-08-05 17:46:18 +0530

    Used mapStateToProps to use in UI

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,17 @@
 import { useEffect } from "react";
 import { connect } from "react-redux";
 import { handleInitialData } from '../actions/shared';
+import Dashboard from "./Dashboard";
 
 const App = (props) => {
   useEffect(() => {
     props.dispatch(handleInitialData());
   }, []);
-  return <div>Starter Code</div>;
+  return <div>{props.loading === true ? null : <Dashboard/>}</div>;
 };
 
-export default connect()(App);
+const mapStateToProps = ({authUser}) => ({
+  loading: authUser === null
+});
+
+export default connect(mapStateToProps)(App);
