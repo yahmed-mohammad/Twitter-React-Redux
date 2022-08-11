32e607b5afeefb0867bc1cc7da81acf930a8e7e3
Mohammad Ahmed <yashminahmed@Yashmins-MacBook-Air.local>
2022-08-11 16:26:17 +0530

    Added Component as Routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,35 @@
-import { useEffect } from "react";
+import { useEffect, Fragment } from "react";
 import { connect } from "react-redux";
 import { handleInitialData } from '../actions/shared';
 import Dashboard from "./Dashboard";
 import LoadingBar from "react-redux-loading-bar";
 import NewTweet from "./NewTweet";
 import TweetPage from "./TweetPage";
+import Nav from "./Nav";
+import { Routes, Route } from "react-router-dom";
 
 const App = (props) => {
   useEffect(() => {
     props.dispatch(handleInitialData());
   }, []);
   return <div>
-    <LoadingBar/>
-      {props.loading === true ? null : <TweetPage match={{
-        params: {id: "2mb6re13q842wu8n106bhk"}
-      }}/>}
+    <Fragment>
+      <LoadingBar/>
+      <div className="container">
+        <Nav/>
+        {
+          props.loading === true ? null : (
+            <Routes>
+                <Route path="/" exact element={<Dashboard/>}/>
+                <Route path="/tweet/:id" exact element={<TweetPage/>}/>
+                <Route path="/new" exact element={<NewTweet/>}/>
+            </Routes>
+          )
+        }
+      </div>
+    </Fragment>
+    
+    
     </div>;
 };
 
