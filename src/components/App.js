c9c0ebf710eafda6b8f5654ac1dc0a17270a234c
Mohammad Ahmed <yashminahmed@Yashmins-MacBook-Air.local>
2022-08-11 12:38:00 +0530

    Added TweetPage

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import { handleInitialData } from '../actions/shared';
 import Dashboard from "./Dashboard";
 import LoadingBar from "react-redux-loading-bar";
 import NewTweet from "./NewTweet";
+import TweetPage from "./TweetPage";
 
 const App = (props) => {
   useEffect(() => {
@@ -11,7 +12,9 @@ const App = (props) => {
   }, []);
   return <div>
     <LoadingBar/>
-      {props.loading === true ? null : <NewTweet/>}
+      {props.loading === true ? null : <TweetPage match={{
+        params: {id: "2mb6re13q842wu8n106bhk"}
+      }}/>}
     </div>;
 };
 
